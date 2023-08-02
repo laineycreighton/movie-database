8fa89990467328f230ec153d98ff7996d16740ef
kayla <kayladatte@gmail.com>
2023-08-02 15:47:12 -0700

    adding what we did in class together

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+USE movies_db;
+
+CREATE TABLE movies (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  movie_name VARCHAR(30) NOT NULL
+);
+
+
